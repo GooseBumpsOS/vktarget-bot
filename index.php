3f15487a786bf0c4dcadbbbdbe1a8ccc4c912472
Georgy Gusev <goosebumpsmail2@gmail.com>
2020-03-25 08:23:01 +0300

    Удалил лишние коментарии

--- index.php
+++ index.php
@@ -16,25 +16,3 @@ $main->loop(60);
 
 //77081267010
 //50dadbb1b3fdc314aa3abd
-//var jq = document.createElement('script');
-//jq.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js";
-//document.getElementsByTagName('head')[0].appendChild(jq);
-//
-//            jQuery.ajax({
-//                url: "/api/all.php",
-//                type: "post",
-//                data: {
-//    action: "auth",
-//                    email: '16c9d9537f@emailtown.club',
-//                    password: '16c9d9537f@emailtown.club',
-//                    js_on: 2020,
-//                    timezone_diff: -20,
-//                    answer_code: ''
-//                },
-//                headers: {
-//    VKN:     $("html").prop("nextSibling").nodeValue.match(/magicbox(.*)magicbox/)[1]
-//},
-//                success: function(e) {
-//alert('ok')
-//}
-//            })
