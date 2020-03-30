949d60fd712e5884fafc7016ff369c4a60dbc4a1
Georgy Gusev <goosebumpsmail2@gmail.com>
2020-03-30 12:02:41 +0300

    Исправил скрипт инициализации

--- Scripts/init.sh
+++ Scripts/init.sh
@@ -4,20 +4,24 @@ var=`date +%s`;
 
 mkdir /home/georgy/vkTargetBots/$var;
 
-cp -r /home/georgy/vkTargetBase /home/georgy/$var/
+cp -r /home/georgy/vkTargetBase/* /home/georgy/vkTargetBots/$var/
 
-echo "[
+echo '[
   {
     "Vk": {
-      "login": "51979581736",
-      "pass": "vv11Fu76B9x",
-      "token": "9102fe8b7a62ab6655d02ef32203e1594c545a5bd51eea84d7494b7fd2264fe44aec0cd8879c7f5a758c9"
+      "login": "959780904914",
+      "pass": "glSp2YupsyeN",
+      "token": "6f3f82d89fc317cff67aeb7e4bcd1765b101182576c01aceb95fd88d24e803dd2bd858d3e0fab7b4f5f9d"
     },
     "Vktarget": {
-      "login": "aleshenka-golikov@bk.ru",
-      "pass": "aleshenka-golikov@bk.ru"
+      "login": "grisha.gukov@mail.ru",
+      "pass": "grisha.gukov@mail.ru"
     }
   }
-]" >  /home/georgy/vkTargetBots/$var/jsondb/Tables/auth.data.json;
+]' >  /home/georgy/vkTargetBots/$var/jsondb/Tables/auth.data.json;
 
-nohup php /home/georgy/vkTargetBots/$var/index.php > /home/georgy/vkTargetBots/$var/logTarget.txt 2>&1 &
+cd /home/georgy/vkTargetBots/$var/;
+
+nohup php index.php >logTarget.txt 2>&1 & echo $! > pid.txt
+
+cd ~;
